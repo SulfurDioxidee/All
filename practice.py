a7a7df8045983580b6202941fd182a5f492c3d81
SulfurDioxide <90202704+SulfurDioxidee@users.noreply.github.com>
2022-06-13 21:58:15 +0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,10 +1,11 @@
 import random
 from time import sleep
-#--Hikmet Ramazanov--
+#Hikmet Ramazanov
+#GitHub >>> https://github.com/SulfurDioxidee
 
 oyuncu_win=0
 kompyuter_win=0
-secimler=["das" , "kagiz" , "qayci" ]
+secimler=["das" , "kagiz" , "qayci", "q" ]
 while True:
     secim=input("Das , Kagiz , Qayci :").lower()
 
@@ -16,9 +17,9 @@ while True:
     print("Senin secimin: ",secim  )
     sleep(1)
     print("1")
-    sleep(2)
+    sleep(1)
     print("2")
-    sleep(3)
+    sleep(1)
     print("3")
     if kompyuter_secim=="das" and secim=="kagiz":
         oyuncu_win=oyuncu_win+1
@@ -56,6 +57,19 @@ while True:
         print("[",kompyuter_win ,"]","[:|]Kompyuter:" , kompyuter_secim , "||", secim , ":Oyuncu[:|]" , "[",oyuncu_win, "]" )
         print("*****************")
         print("*****************")
+    if secim =="q":
+        if oyuncu_win > kompyuter_win:
+            print("Qazandin !")
+            print("Sen: ", oyuncu_win , "Kompyuter: ", kompyuter_win)
+            break
+        if oyuncu_win == kompyuter_win:
+            print("Beraber")
+            print("Sen: ", oyuncu_win , "Kompyuter: ", kompyuter_win)
+            break
+        else:
+            print("Uduzdun!")
+            print("Sen: ", oyuncu_win , "Kompyuter: ", kompyuter_win)
+            break
         
     else:
         kompyuter_win=kompyuter_win+1
